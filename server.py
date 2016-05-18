50e4d77a8b951adedc752a5cd07238125a80ee8a
Adam Kaliski <adam.kaliski47@gmail.com>
2016-05-18 20:08:25 +0200

    Server now displays non-zero processes, CPU and memory percentage use

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+#author Adam Kaliski adam.kaliski47@gmail.com
 from flask import Flask
 import psutil
 
@@ -5,12 +6,23 @@ app = Flask(__name__)
 @app.route('/')
 
 def sysinf():
-	cpuperc = psutil.cpu_percent(interval=0)
 	cpuperc = psutil.cpu_percent(interval=1)
-	result = "The CPU percentage usage is " + str(cpuperc) + "%"
+	cpuresult = "The CPU percentage usage is " + str(cpuperc) + "%"
 	memperc = psutil.virtual_memory()[2]
 	memresult = "The memory percentage usage is " + str(memperc) + "%" 
-	finalres = result + "<br/>" + memresult
+	pslist = []
+	for proc in psutil.process_iter():
+		cpuproc = proc.cpu_percent(interval=1)
+		if(cpuproc == 0):
+			continue
+		else:
+			pslist.append([proc.pid, cpuproc])
+	pslist.sort(key=lambda x: x[1])#sort by percentage use
+	proc_fin = ""
+	for ps in reversed(pslist):#reversed because sort sorts it by descending
+		p = psutil.Process(ps[0])
+		proc_fin += str(ps[0]) + " : " +p.name()+" : CPU percentage: "+str(ps[1]) + "<br/>"
+	finalres = cpuresult + "<br/>" + memresult +"<br/>"+proc_fin
 	return finalres
 		
 if __name__ == '__main__':
